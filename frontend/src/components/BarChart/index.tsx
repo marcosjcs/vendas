861d1ab59bfb386550931e3b0fdcbf5a1e1cd71c
marcosjcs <jcs.marcos13@gmail.com>
2021-09-10 19:55:39 -0300

    BarChart integration

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,10 +1,56 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import Chart from 'react-apexcharts';
+import { SaleSuccess } from 'types/sale';
+import { round } from 'utils/format';
+import { BASE_URL } from 'utils/requests';
 
 interface Props {
   theme: string;
 }
 
+type SeriesData = {
+  name: string;
+  data: number[];
+};
+
+type ChartData = {
+  labels: {
+    categories: string[];
+  },
+  series: SeriesData[];
+  };
+
 const BarChart: React.FC<Props> = ({ theme }) => {
+  const [chartData, setChartData] = useState<ChartData>({
+    labels: {
+      categories: []
+    },
+    series: [
+      {
+        name: "",
+        data: []                   
+      }
+    ]});
+
+    useEffect(() => {
+      axios.get(`${BASE_URL}/sales/success-by-seller`).then(response => {
+        const data = response.data as SaleSuccess[];
+        const myLabels = data.map(x => x.sellerName);
+        const mySeries = data.map(x => round(100.0 * (x.deals / x.visited),1));
+  
+        setChartData({
+          labels: {
+            categories: myLabels
+          },
+          series: [
+            {
+              name: "% Sucesso",
+              data: mySeries                 
+            }
+        ]});
+      });
+    },[]);
   const options = {
     plotOptions: {
       bar: {
@@ -13,22 +59,10 @@ const BarChart: React.FC<Props> = ({ theme }) => {
     },
   };
 
-  const mockData = {
-    labels: {
-      categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    },
-    series: [
-      {
-        name: "% Sucesso",
-        data: [43.6, 67.1, 67.7, 45.6, 71.1]                   
-      }
-    ]
-  };
-
   return (
     <Chart
-      options={{ ...options, xaxis: mockData.labels, theme: {mode: theme === "light" ? "light" : "dark"} }}
-      series={mockData.series}
+      options={{ ...options, xaxis: chartData.labels, theme: {mode: theme === "light" ? "light" : "dark"} }}
+      series={chartData.series}
       type="bar"
       height="240"
     />
