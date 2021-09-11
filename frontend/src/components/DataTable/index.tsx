930bc1db634380051515a95933c7050a41036f99
marcosjcs <jcs.marcos13@gmail.com>
2021-09-10 22:06:13 -0300

    Pagination

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Pagination from "components/Pagination";
 
 import { SalePage } from "types/sale";
 import { formatLocalDate } from "utils/format";
@@ -6,6 +7,7 @@ import { formatLocalDate } from "utils/format";
 import api from "services/api";
 
 const DataTable = () => {
+  const [activePage, setActivePage] = useState(0);
   const [page, setPage] = useState<SalePage>({
     first: true,
     last: true,
@@ -15,37 +17,43 @@ const DataTable = () => {
   });
 
   useEffect(() => {
-    api.get('sales?page=0&size=5&sort=date,desc').then(response => {
+  api.get(`sales?page=${activePage}&size=5&sort=date,desc`).then(response => {
       setPage(response.data);
     });
-  },[]);
+  },[activePage]);
+
+  const changePage = (index: number) => {
+    setActivePage(index);
+  }
 
   return (
-    <div className="table-responsive">
-      <table className="table table-striped table-sm">
-        <thead>
-          <tr>
-            <th>Data</th>
-            <th>Vendedor</th>
-            <th>Clientes visitados</th>
-            <th>Negócios fechados</th>
-            <th>Valor</th>
-          </tr>
-        </thead>
-        <tbody>
-          {page.content?.map(sale => (
-            <tr key={sale.id}>
-              <td>{formatLocalDate(sale.date,'dd/MM/yyyy')}</td>
-              <td>{sale.seller.name}</td>
-              <td>{sale.visited}</td>
-              <td>{sale.deals}</td>
-              <td>{Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'}).format(sale.amount)}</td>
+    <>
+      <Pagination page={page} onPageChange={changePage}/>
+      <div className="table-responsive">
+        <table className="table table-striped table-sm">
+          <thead>
+            <tr>
+              <th>Data</th>
+              <th>Vendedor</th>
+              <th>Clientes visitados</th>
+              <th>Negócios fechados</th>
+              <th>Valor</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-
+          </thead>
+          <tbody>
+            {page.content?.map(sale => (
+              <tr key={sale.id}>
+                <td>{formatLocalDate(sale.date,'dd/MM/yyyy')}</td>
+                <td>{sale.seller.name}</td>
+                <td>{sale.visited}</td>
+                <td>{sale.deals}</td>
+                <td>{Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'}).format(sale.amount)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
 
---
+++ frontend/src/components/Pagination/index.tsx
@@ -0,0 +1,34 @@
+import { SalePage } from "types/sale";
+
+type Props = {
+  page: SalePage;
+  onPageChange: Function;
+}
+
+const Pagination = ( {page, onPageChange} : Props ) => {
+  return (
+    <div className="d-flex justify-content-center">
+      <nav>
+        <ul className="pagination">
+          <li className={`page-item ${page.number === 0 ? 'disabled' : ''}`}>
+            <button className="page-link" onClick={() => onPageChange(0)}>{'<<'} Primeira</button>
+          </li>
+          <li className={`page-item ${page.first ? 'disabled' : ''}`}>
+            <button className="page-link" onClick={() => onPageChange(page.number - 1)}>{'<'} Anterior</button>
+          </li>
+          <li className="page-item disabled">
+            <span className="page-link">{page.number + 1}</span>
+          </li>
+          <li className={`page-item ${page.last ? 'disabled' : ''}`}>
+            <button className="page-link" onClick={() => onPageChange(page.number + 1)}>Próxima {'>'}</button>
+          </li>
+          <li className={`page-item ${page.number === page.totalPages - 1 ? 'disabled' : ''}`}>
+            <button className="page-link" onClick={() => onPageChange(page.totalPages - 1)}>Última {'>>'}</button>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
+};
+
+export default Pagination;
